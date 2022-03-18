d84abb67fc75de79764a6d21330bbfc75b2358aa
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2022-03-18 13:11:11 +0300

    Add files via upload

---
+++ pp1/pp1/pp1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <thread>
+using namespace std;
+
+void test() {
+	int n = 2147483647;
+	double ln2 = 0;
+	double test;
+	double start = omp_get_wtime();
+
+#pragma omp parallel
+	{
+		#pragma omp for private (test), reduction (+:ln2)
+		for (int i = 1; i < n; i++)
+		{
+			test = 0;
+			test += (double)((i % 2 == 0) ? -1 : 1) / i;
+			ln2 += test;
+		}
+	}
+	printf("time = %lf\nln2=%.40lf", omp_get_wtime() - start, ln2);
+}
+
+int main()
+{
+	thread thd(test);
+	thd.join();
+	
+}
+
