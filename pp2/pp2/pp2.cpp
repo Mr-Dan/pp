907816b4b6cf1894ed5d8776b754bc4bc2335228
Mr-Dan <35195104+Mr-Dan@users.noreply.github.com>
2022-03-18 13:14:11 +0300

    Add files via upload

---
+++ pp2/pp2/pp2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main()
+{
+	FILE* fp;
+	 fopen_s(&fp,"S1.txt", "w");
+
+	double start = omp_get_wtime();	
+	int sum = 0;
+	int n = 1000;
+		
+	#pragma omp parallel for reduction(+:sum) 
+		for (int x = -n; x <= n; x++)
+		{
+			for (int y = -n; y <= n; y++)
+			{
+				for (int z = -n; z <= n; z++)
+				{
+					if (x + y + z == n)
+					{
+						fprintf(fp, "%d %d %d %d\n", x, y, z);
+						sum ++;
+					}
+				}
+			}
+		}
+
+	fclose(fp);
+	printf("time = %lf\nsum2=%d", omp_get_wtime() - start, sum);
+	
+}
+
+
